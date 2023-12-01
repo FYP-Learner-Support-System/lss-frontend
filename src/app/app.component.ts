ece2074bc37835641a970b27112d6b35f2543d55
abdulrafay199 <abdulrafay12364@gmail.com>
2023-12-01 16:27:03 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatChipsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

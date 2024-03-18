1954d3198b502b99f7ff8a51d83965789be8dc42
abdul rafay <abdulrafay12364@gmail.com>
2024-03-18 04:01:42 -0700

    changed domain to deployed API

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -13,7 +13,7 @@ export class ChatService {
 
   getResponse(body: any): Observable<any>{
     // const headers = new HttpHeaders().set('Authorization', `${token}`).set('Content-Type', 'application/json');
-    return this.http.post<any>(`${this.domain1}/get_response/`,body, {observe: 'response' })
+    return this.http.post<any>(`${this.domain}/get_response/`,body, {observe: 'response' })
       .pipe(
         catchError((error: HttpErrorResponse) => {
           return throwError({ status: error.status, message: error.error }); // Forward the error to the caller

2ee3265c6fa22315e4c627145e6dd2328d22add7
abdul rafay <abdulrafay12364@gmail.com>
2024-03-18 13:30:21 -0700

    home bg image optimized using pixboost

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,7 +9,11 @@ import { reducers, metaReducers } from './store/reducers';
 import { FormsModule, NgModel } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideRouter(routes1), provideAnimations(), provideStore(reducers, { metaReducers }),provideHttpClient(),MessageService]
+  providers: [provideRouter(routes), provideRouter(routes1), provideAnimations(), provideStore(reducers, { metaReducers }), provideHttpClient(), MessageService, provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
